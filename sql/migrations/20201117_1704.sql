75f9174eb1d941a0681d47ee5c9554e2a1b8b1a4
Pierre Delattre <pierredelattre@MacBook-Pro-de-Pierre.local>
2020-11-17 17:34:19 +0100

    sql

--- sql/migrations/20201117_1704.sql
+++ sql/migrations/20201117_1704.sql
@@ -1,13 +1,12 @@
-CREATE DATABASE IF NOT EXISTS `CHANSONS` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `CHANSONS`;
+USE `mspr`;
 
-CREATE TABLE `ARTISTE` (
+CREATE TABLE `artiste` (
   `artiste_id` VARCHAR(42),
   `nom` VARCHAR(42),
   PRIMARY KEY (`artiste_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `CERTIFICATION` (
+CREATE TABLE `certification` (
   `certification_id` VARCHAR(42),
   `titre` VARCHAR(42),
   `date_obtention` VARCHAR(42),
@@ -15,25 +14,25 @@ CREATE TABLE `CERTIFICATION` (
   PRIMARY KEY (`certification_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `ECRIT` (
+CREATE TABLE `ecrit` (
   `chanson_id` VARCHAR(42),
   `artiste_id` VARCHAR(42),
   PRIMARY KEY (`chanson_id`, `artiste_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `COMPOSE` (
+CREATE TABLE `compose` (
   `album_id` VARCHAR(42),
   `artiste_id` VARCHAR(42),
   PRIMARY KEY (`album_id`, `artiste_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `EST_DELIVREE_A` (
+CREATE TABLE `est_delivree_a` (
   `album_id` VARCHAR(42),
   `certification_id` VARCHAR(42),
   PRIMARY KEY (`album_id`, `certification_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `UTILISATEUR` (
+CREATE TABLE `utilisateur` (
   `user_id` VARCHAR(42),
   `pseudo` VARCHAR(42),
   `mail` VARCHAR(42),
@@ -41,13 +40,13 @@ CREATE TABLE `UTILISATEUR` (
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `AJOUTE` (
+CREATE TABLE `ajoute` (
   `user_id` VARCHAR(42),
   `chanson_id` VARCHAR(42),
   PRIMARY KEY (`user_id`, `chanson_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `CHANSON` (
+CREATE TABLE `chanson` (
   `chanson_id` VARCHAR(42),
   `nom` VARCHAR(42),
   `date_de_sortie` VARCHAR(42),
@@ -58,7 +57,7 @@ CREATE TABLE `CHANSON` (
   PRIMARY KEY (`chanson_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `ALBUM` (
+CREATE TABLE `album` (
   `album_id` VARCHAR(42),
   `nom` VARCHAR(42),
   `date_de_sortie` VARCHAR(42),
@@ -71,28 +70,28 @@ CREATE TABLE `ALBUM` (
   PRIMARY KEY (`album_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `CLASSEE_DANS` (
+CREATE TABLE `classee_dans` (
   `classement_id` VARCHAR(42),
   `chanson_id` VARCHAR(42),
   `date_insertion` VARCHAR(42),
   PRIMARY KEY (`classement_id`, `chanson_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `CLASSEMENT` (
+CREATE TABLE `classement` (
   `classement_id` VARCHAR(42),
   `pays` VARCHAR(42),
   `genre` VARCHAR(42),
   PRIMARY KEY (`classement_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `ECRIT` ADD FOREIGN KEY (`artiste_id`) REFERENCES `ARTISTE` (`artiste_id`);
-ALTER TABLE `ECRIT` ADD FOREIGN KEY (`chanson_id`) REFERENCES `CHANSON` (`chanson_id`);
-ALTER TABLE `COMPOSE` ADD FOREIGN KEY (`artiste_id`) REFERENCES `ARTISTE` (`artiste_id`);
-ALTER TABLE `COMPOSE` ADD FOREIGN KEY (`album_id`) REFERENCES `ALBUM` (`album_id`);
-ALTER TABLE `EST_DELIVREE_A` ADD FOREIGN KEY (`certification_id`) REFERENCES `CERTIFICATION` (`certification_id`);
-ALTER TABLE `EST_DELIVREE_A` ADD FOREIGN KEY (`album_id`) REFERENCES `ALBUM` (`album_id`);
-ALTER TABLE `AJOUTE` ADD FOREIGN KEY (`chanson_id`) REFERENCES `CHANSON` (`chanson_id`);
-ALTER TABLE `AJOUTE` ADD FOREIGN KEY (`user_id`) REFERENCES `UTILISATEUR` (`user_id`);
-ALTER TABLE `ALBUM` ADD FOREIGN KEY (`chanson_id`) REFERENCES `CHANSON` (`chanson_id`);
-ALTER TABLE `CLASSEE_DANS` ADD FOREIGN KEY (`chanson_id`) REFERENCES `CHANSON` (`chanson_id`);
-ALTER TABLE `CLASSEE_DANS` ADD FOREIGN KEY (`classement_id`) REFERENCES `CLASSEMENT` (`classement_id`);
+ALTER TABLE `ecrit` ADD FOREIGN KEY (`artiste_id`) REFERENCES `artiste` (`artiste_id`);
+ALTER TABLE `ecrit` ADD FOREIGN KEY (`chanson_id`) REFERENCES `chanson` (`chanson_id`);
+ALTER TABLE `compose` ADD FOREIGN KEY (`artiste_id`) REFERENCES `artiste` (`artiste_id`);
+ALTER TABLE `compose` ADD FOREIGN KEY (`album_id`) REFERENCES `album` (`album_id`);
+ALTER TABLE `est_delivree_a` ADD FOREIGN KEY (`certification_id`) REFERENCES `certification` (`certification_id`);
+ALTER TABLE `est_delivree_a` ADD FOREIGN KEY (`album_id`) REFERENCES `ALBUM` (`album_id`);
+ALTER TABLE `ajoute` ADD FOREIGN KEY (`chanson_id`) REFERENCES `chanson` (`chanson_id`);
+ALTER TABLE `ajoute` ADD FOREIGN KEY (`user_id`) REFERENCES `utilisateur` (`user_id`);
+ALTER TABLE `album` ADD FOREIGN KEY (`chanson_id`) REFERENCES `chanson` (`chanson_id`);
+ALTER TABLE `classee_dans` ADD FOREIGN KEY (`chanson_id`) REFERENCES `chanson` (`chanson_id`);
+ALTER TABLE `classee_dans` ADD FOREIGN KEY (`classement_id`) REFERENCES `classement` (`classement_id`);
