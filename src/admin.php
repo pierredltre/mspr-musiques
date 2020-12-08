6f8bb08e573147ac58a65e3a58fec3b0845304fb
Pierre Delattre <pierredelattre@MacBook-Pro-de-Pierre.local>
2020-12-08 13:01:35 +0100

    add bootstrap

--- src/admin.php
+++ src/admin.php
@@ -1,58 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Administration</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+
 <?php
 
 require_once('connexion.php');
 $tables = ['ALBUM', 'ARTISTE', 'CHANSON', 'COMPOSE', 'ECRIT'];
 $sql = "";
 
-// foreach ($tables as $table) {
-//     $sql .= "SELECT * FROM :tableaux";
-//     $result = $dbh->prepare($sql);
-//     $result->bindParam(':tableaux', $table);
-// }
-
-// $result->execute();
-// echo "<pre>";
-// echo($result);
-// echo "</pre>";
-
 foreach ($tables as $table) {
+    echo "<div class=\"row\">";
     $donnees = $dbh->query("SELECT * FROM $table");
+    echo "<div class=\"col-lg-6\">";
     echo $table;
+    
     echo("<pre>");
     foreach ($donnees->fetchAll(PDO::FETCH_ASSOC) as $colonnes) {
         // print_r(array_keys($colonnes));
         print_r($colonnes);
-        // echo $colonnes['nom'];
-
-        if (array_key_exists("chanson_id", $colonnes)) {
-            formulaireChanson();
-        }
-
+        echo "</div>";
+        echo "<div class=\"col-lg-6\">";
+        // if (array_key_exists("chanson_id", $colonnes) && array_key_exists("chanson_id", $colonnes)) {
+        //     formChanson();
+        // }
         if ($table == "CHANSON") {
-            formulaireChanson();
+            // echo(formChanson());
         } else if ($table == "ALBUM") {
-            // formulaireChanson();
+            echo(formAlbum());
         } else if ($table == "ARTISTE") {
             // formulaireChanson();
         }
+        echo "</div>";
     }
     echo("<pre>");
+    echo "</div>";
 }
-// Refaire function
 
-// function formulaireChanson() {
-//     echo "<br>";
-//     echo "<h3>Ajouter une chanson</h3>";
-//     echo "<form action=\"#\" method=\"post\" enctype=\"multipart/form-data\">";
-//     echo "<label>Nom de la chanson</label> <br>";
-//     echo "<input></input> <br>";
-//     echo "<label>Date de sortie</label> <br>";
-//     echo "<input type=\"date\"></input> <br>";
-//     echo "<label>Cover</label> <br>";
-//     echo "<input type=\"file\"></input> <br>";
-//     echo "<label>Nombre d'écoutes</label> <br>";
-//     echo "<input></input> <br>";
-//     echo "<input type=\"submit\"></select> <br>";
-//     echo "</form>";
-//     echo "<br>";
-// }
+function formAlbum() {
+    // HEREDOC php
+    $html = <<<EOT
+        <form action="create.php" method="post" enctype="multipart/form-data">
+        <br>
+        <h3>Ajouter un Album</h3>
+        <label>Nom de l'album</label> <br>
+        <input name="titreAlbum" class="form-control" required></input> <br>
+        <label>Date de sortie</label> <br>
+        <input name="dateAlbum" class="form-control" type="date" required></input> <br>
+        <label>Cover</label> <br>
+        <input name="coverAlbum" class="form-control-file" type="file"></input> <br>
+        <label>Nombre d'écoutes</label> <br>
+        <input name="streamsAlbum" class="form-control" type="number" required></input> <br>
+        <label>Genre</label>
+        <select class="form-control" name="genreAlbum">
+            <option value="rapfr">Rap FR</option>
+            <option value="rapus">Rap US</option>
+            <option value="rnb">RNB</option>
+            <option value="jazz">Jazz</option>
+        </select> <br>
+        <label>Pays</label>
+        <select class="form-control" name="paysAlbum">
+            <option value="FR">France</option>
+            <option value="US">États-Unis</option>
+            <option value="IT">Italie</option>
+            <option value="DE">Allemagne</option>
+            <option value="UK">Royaume-Uni</option>
+        </select> <br>
+        <label>Single</label>
+        <select class="form-control" name="single">
+            <option value="true">Oui</option>
+            <option value="false">Non</option>
+        </select><br>
+        <input type="submit"></select> <br>
+        </form>
+        <br>
+    EOT;
+ 
+    return $html;
+ }
+
+?>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>    
+</body>
+</html>
---
+++ src/create.php
@@ -0,0 +1,56 @@
+<?php
+require_once './connexion.php';
+
+if (!empty($_POST)) {
+    $titreAlbum = $_POST['titreAlbum'];
+    $dateAlbum = $_POST['dateAlbum'];
+    $streamsAlbum = $_POST['streamsAlbum'];
+    $genreAlbum = $_POST['genreAlbum'];
+    $paysAlbum = $_POST['paysAlbum'];
+    $single = $_POST['single'];
+  
+    if(empty($_FILES['coverAlbum'])){
+      $cover = " ";
+    } else{
+      $cover = basename($_FILES['coverAlbum']['name']);
+    }  
+    $err = 0;
+    if (empty($titreAlbum))
+      $err = 1;
+    if (empty($dateAlbum))
+      $err = 1;
+    if (empty($streamsAlbum))
+      $err = 1;
+    if (empty($genreAlbum))
+      $err = 1;
+    if (empty($paysAlbum))
+      $err = 1;
+    if (empty($single))
+      $err = 1;
+
+    if ($err == 0) {
+      $cover = str_replace(" ", "", $cover);
+      $sql = $dbh->prepare("INSERT INTO `ALBUM` (`nom`, `date_de_sortie`, `cover`, `nombre_ecoutes`, `genre`, `pays`, `single`) VALUES (:nom, :date_de_sortie, :cover, :nombre_ecoutes, :genre, :pays, :single)");
+      $sql->bindParam(':nom', $titreAlbum);
+      $sql->bindParam(':date_de_sortie', $dateAlbum);
+      $sql->bindParam(':cover', $cover);
+      $sql->bindParam(':nombre_ecoutes', $streamsAlbum);
+      $sql->bindParam(':genre', $genreAlbum);
+      $sql->bindParam(':pays', $paysAlbum);
+      $sql->bindParam(':single', $single);
+  
+      if (!empty($_FILES['cover'])) {
+        $uploadDir = '../uploads/';
+        move_uploaded_file($_FILES['cover']['tmp_name'],$uploadDir.$cover);
+      }
+      if ($sql->execute()) {
+        header("Location:./admin.php");
+      } else {
+        exit('Erreur bdd');
+      }
+    } else {
+      $msg = "Veuillez remplir tout les champs";
+      header("Location:./admin.php?msg=$msg");
+    }
+  }
+  ?>
